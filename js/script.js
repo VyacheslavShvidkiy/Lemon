fb10ed343df5a10a48d740fe5c55013d623a96a9
Vyacheslav <slashdev1983@gmail.com>
2023-02-04 16:33:40 +0200

    fix menu burger

--- js/script.js
+++ js/script.js
@@ -4,3 +4,9 @@ contactBurger.addEventListener('click', () => {
     contactBurger.classList.toggle('active');
     contactList.classList.toggle('active');
 })
+let menuBurger = document.querySelector(".menu_burger");
+let menuList =  document.querySelector(".nav_list");
+menuBurger.addEventListener('click', () => {
+    menuBurger.classList.toggle('active');
+    menuList.classList.toggle('active');
+})
