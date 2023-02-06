902bac80f5db182065841703b0e391d206ba2762
Vyacheslav <slashdev1983@gmail.com>
2023-02-06 12:38:38 +0200

    Add "Subscribe" form and addaptive for all blocks

--- js/script.js
+++ js/script.js
@@ -12,3 +12,18 @@ menuBurger.addEventListener('click', () => {
     menuList.classList.toggle('active');
 })
 
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    let panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
