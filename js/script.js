e5fb993165f3c0e6d3bcf4bc9245c4d033ac82f6
Vyacheslav <slashdev1983@gmail.com>
2023-02-06 19:48:09 +0200

    Fix addaptive munu bugs

--- js/script.js
+++ js/script.js
@@ -28,3 +28,55 @@ for (i = 0; i < acc.length; i++) {
     }
   });
 }
+
+const dots = document.querySelectorAll('.dot');
+const slides = document.querySelectorAll('.slide');
+
+let index = 0;
+const activeSlide = n =>{
+    for(slide of slides){
+        slide.classList.remove('activer');
+    }
+    slides[n].classList.add('activer');
+}
+const activeDote = n =>{
+    for(dot of dots){
+        dot.classList.remove('activer');
+    }
+    dots[n].classList.add('activer');
+}
+const prepareCurrentSlide = ind => {
+    activeSlide(index);
+    activeDote(index);
+}
+const nextSlide = () => {
+    if(index == slides.length-1){
+        index = 0;
+        prepareCurrentSlide(index);
+    }else{
+        index++;
+        prepareCurrentSlide(index);
+    }    
+}
+
+const prevSlide = () => {
+    if(index == 0){
+        index = slides.length-1;
+        prepareCurrentSlide(index);
+    }else{
+        index--;
+        prepareCurrentSlide(index);
+    }    
+}
+
+dots.forEach((item, indexDot) => {
+    item.addEventListener('click',() => {
+        index = indexDot;
+        prepareCurrentSlide(index);
+    })
+})
+
+window.onload = () => {
+    setInterval(nextSlide, 1000);
+}
+
