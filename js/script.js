75f0fa051433c3a4158703e3be6f15ae6c24ea28
Vyacheslav <slashdev1983@gmail.com>
2023-02-06 15:52:48 +0200

    Add slider to Testimonial block

--- js/script.js
+++ js/script.js
@@ -9,7 +9,7 @@ contactBurger.addEventListener('click', () => {
 let menuBurger = document.querySelector(".menu_burger");
 let menuList =  document.querySelector(".nav_list");
 menuBurger.addEventListener('click', () => {
-    menuBurger.classList.toggle('active');
+    menuBurger.classList.toggle('activ');
     menuList.classList.toggle('active');
 })
 
@@ -19,7 +19,7 @@ let i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    this.classList.toggle("active");
+    this.classList.toggle("activated");
     let panel = this.nextElementSibling;
     if (panel.style.display === "block") {
       panel.style.display = "none";
